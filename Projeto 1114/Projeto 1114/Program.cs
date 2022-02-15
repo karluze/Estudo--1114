4085e877457380871c03034436964e921843cdc6
karluze <karluzem@gmail.com>
2022-02-15 16:55:32 -0300

    Projeto 1114

---
+++ Projeto 1114/Projeto 1114/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+
+            senha = int.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+
+
+            //Console.ReadLine();
+        }
+    }
+}
